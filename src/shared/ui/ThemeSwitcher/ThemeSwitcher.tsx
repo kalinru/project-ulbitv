4061d0afa208113dc79b6542bd62bc5fa2b721fe
ruslan <test17529127590872198739012730912794@gmail.com>
2023-12-03 20:31:14 +0500

    fix 7

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,7 +3,6 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
-import cls from './ThemeSwitcher.module.scss'
 import { Button, ThemeButton } from 'shared/ui'
 
 interface ThemeSwitcherProps {
@@ -18,7 +17,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = (props) => {
     <Button
           theme={ThemeButton.CLEAR}
           onClick={toggleTheme}
-          className={classNames(cls.ThemeSwitcher, {}, [className])}>
+          className={classNames('', {}, [className])}>
       { theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon /> }
     </Button>
   )
